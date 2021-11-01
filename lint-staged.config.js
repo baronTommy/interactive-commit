51531da8db4eb15adeb924aef8861030400366fe
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-11-01 13:35:06 +0900

    perf: :heavy_minus_sign: fix packages, script

--- lint-staged.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "*.json": ["npm run qualityCheck:fmt"],
-  "*.md": ["npm run qualityCheck:fmt"],
-  "*.yml": ["npm run qualityCheck:fmt"],
-  "*.js": ["npm run qualityCheck:lint", "npm run qualityCheck:fmt"],
-  "*.{ts,tsx}": ["npm run qualityCheck:lint", "npm run qualityCheck:fmt"],
-};
