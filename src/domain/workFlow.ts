84c4507ace9c6923174171d6b9e9a082fee815f6
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-11-02 16:14:57 +0900

    refactor(domain): :recycle: refactor dir

--- src/domain/workFlow.ts
+++
@@ -1,8 +0,0 @@
-import type { Answer, Question, Setting } from "./core";
-
-export type GetQuestion = (p: Setting) => Question | undefined;
-export type UpdateTemplate = (p: {
-  template: Setting["template"];
-  searchValue: string;
-  answer: Answer;
-}) => Setting["template"];
